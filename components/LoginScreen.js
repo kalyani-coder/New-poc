a63bcdd1981ec53ca85967982b1f87544470f206
kalyani murumkar <kalyanimurumkar@kalyanis-MacBook-Air.local>
2024-02-01 17:14:54 +0530

    adding login page

--- components/LoginScreen.js
+++ components/LoginScreen.js
@@ -2,41 +2,41 @@ import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 import tw from 'twrnc';
 import React, { useState } from 'react';
 
-
 const LoginScreen = () => {
+  const [userId, setUserId] = useState('');
+  const [password, setPassword] = useState('');
 
-  const [userId , setUserId ] = useState()
-  const [password , setpassword ] = useState()
-
+  const handleSubmit = () => {
+    const data = {
+      userId,
+      password,
+    };
+    console.log(data);
+  };
 
   return (
-    <View style={tw`flex-1 justify-center items-center bg-gray-100`}>
+    <View style={tw`flex-1  bg-gray-100`}>
+      <View style={tw`mt-10 pl-4`}>
+        <Text style={tw`text-xl font-bold mb-4`}>Welcome to CRN</Text>
+        <Text style={tw`text-xl font-bold mb-4`}>Login to Your Account</Text>
+      </View>
       <View style={tw`p-4`}>
-      <Text style={tw`text-3xl font-bold mb-4 self-center`}>Login</Text>
-
-
         <TextInput
-          style={tw`border border-gray-300 p-2 w-90 mb-4 rounded-md text-black`}
+          style={tw`border border-gray-300 p-2 w-80 mb-4 rounded-md text-black self-center`}
           placeholder="User Id"
-          placeholderTextColor="#000"
-          onChange={(e)=>{
-            setUserId(e.target.value)
-          }}
+          onChangeText={(text) => setUserId(text)}
+          value={userId}
         />
         <TextInput
-          style={tw`border border-gray-300 p-2 w-90 mb-4 rounded-md text-black`}
+          style={tw`border border-gray-300 p-2 w-80 mb-4 rounded-md text-black self-center`}
           placeholder="Password"
           secureTextEntry={true}
-          placeholderTextColor="#000"
-          onChange={(e)=>{
-            setpassword(e.target.value)
-          }}
+          onChangeText={(text) => setPassword(text)}
+          value={password}
         />
         <TouchableOpacity
-          style={tw`p-2 w-90 mb-4 rounded-md bg-blue-300 self-center`}
-          onPress={() => {
-            // Handle login logic here
-          }}>
+          style={tw`p-2 w-80 mb-4 rounded-md bg-blue-300 self-center`}
+          onPress={handleSubmit}>
           <Text style={tw`text-black text-center`}>Login</Text>
         </TouchableOpacity>
       </View>
