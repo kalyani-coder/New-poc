64db546d77863a755bf1263d7de65b00bb858002
kalyani murumkar <kalyanimurumkar@kalyanis-MacBook-Air.local>
2024-02-01 13:58:22 +0530

    adding some pages

--- App.tsx
+++ App.tsx
@@ -2,13 +2,14 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import LoginScreen from './components/LoginScreen'
 import CreateNewRequest from './components/CreateNewRequest'
+import ClientDetails from './components/ClientDetails'
+import Region from './components/Region'
 
 const App = () => {
   return (
     <View>
       
-      <LoginScreen/>
-      <CreateNewRequest/>
+   <Region/>
     </View>
   )
 }
